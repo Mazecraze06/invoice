cf9afebff1c1d5780709bf2903513ed5ebd70b4f
skuzzymiglet <skuzzymiglet@gmail.com>
2020-03-11 16:25:40 +0000

    Add basic Tk window

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import tkinter
 from tkinter import *
 
 window = tkinter.Tk()
+window.title("invoice")
 window.geometry("500x500")
+
+
+
+title = Label(window, text = "Invoice")
+title.grid(row = 0, column = 0, sticky = N+W)
 
 window.mainloop()
