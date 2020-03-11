5d05863cc9994f41572fd3e3edd81f73cf7b37ee
skuzzymiglet <skuzzymiglet@gmail.com>
2020-03-11 16:27:42 +0000

    Revert "Add basic Tk window"
    This reverts commit cf9afebff1c1d5780709bf2903513ed5ebd70b4f.

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import tkinter
 from tkinter import *
 
 window = tkinter.Tk()
-window.title("invoice")
 window.geometry("500x500")
-
-
-
-title = Label(window, text = "Invoice")
-title.grid(row = 0, column = 0, sticky = N+W)
 
 window.mainloop()
